576b1743b56afdeec482ff50fc5f56151893f94e
WybsonSantana <wybson@hotmail.com>
2022-12-27 21:33:39 -0300

    kFoods - 02.16 - Ambiguidade de beans e injeção de lista de beans

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/notificacao/NotificadorSMS.kt
@@ -0,0 +1,12 @@
+package br.dev.s2w.kfoods.api.di.notificacao
+
+import br.dev.s2w.kfoods.api.di.modelo.Cliente
+import org.springframework.stereotype.Component
+
+@Component
+class NotificadorSMS : Notificador {
+
+    override fun notificar(cliente: Cliente, mensagem: String) {
+        println("Notificando ${cliente.nome} através do telefone ${cliente.telefone}: $mensagem")
+    }
+}
--- src/main/kotlin/br/dev/s2w/kfoods/api/di/service/AtivacaoClienteService.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/service/AtivacaoClienteService.kt
@@ -8,16 +8,14 @@ import org.springframework.stereotype.Component
 @Component
 class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private var notificador: Notificador? = null
+    @Autowired
+    private var notificadores: List<Notificador>? = null
 
     fun ativar(cliente: Cliente) {
         cliente.ativar()
 
-        if (notificador != null) {
-            notificador!!.notificar(cliente, "Seu cadastro no sistema agora está ativo!")
-        } else {
-            println("O cliente ${cliente.nome} foi ativado no sistema, mas não foi notificado devido a falta de um serviço de notificação.")
+        for (notificador in notificadores!!) {
+            notificador.notificar(cliente, "Seu cadastro no sistema agora está ativo!")
         }
     }
 
