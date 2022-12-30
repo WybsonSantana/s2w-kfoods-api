7d08c1f42dc0819ebb6047867df5cf88b2e152d2
WybsonSantana <wybson@hotmail.com>
2022-12-30 16:13:06 -0300

    kFoods - 02.24 - Substituindo propriedades via linha de comando e variáveis de ambiente

--- src/test/kotlin/br/dev/s2w/kfoods/api/S2wKfoodsApiApplicationTests.kt
+++ src/test/kotlin/br/dev/s2w/kfoods/api/S2wKfoodsApiApplicationTests.kt
@@ -1,13 +1,16 @@
 package br.dev.s2w.kfoods.api
 
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
+@RunWith(SpringRunner::class)
 @SpringBootTest
 class S2wKfoodsApiApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
