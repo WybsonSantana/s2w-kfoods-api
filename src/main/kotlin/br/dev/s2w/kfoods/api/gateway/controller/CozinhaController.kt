35effe2370b07d042b5524192b560a8a4ff1e110
WybsonSantana <wybson@hotmail.com>
2023-01-06 01:54:14 -0300

    kFoods - 04.13 - Implementando content negotiation para retornar JSON ou XML

--- src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
@@ -2,18 +2,26 @@ package br.dev.s2w.kfoods.api.gateway.controller
 
 import br.dev.s2w.kfoods.api.domain.model.Cozinha
 import br.dev.s2w.kfoods.api.domain.repository.CozinhaRepository
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping("/cozinhas") /* , [produces = MediaType.APPLICATION_XML_VALUE]) */
 class CozinhaController(
     private val cozinhaRepository: CozinhaRepository
 ) {
 
-    @GetMapping
-    fun listar(): List<Cozinha> {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun listarJson(): List<Cozinha> {
+        println("listarJson()")
+        return cozinhaRepository.listar()
+    }
+
+    @GetMapping(produces = [MediaType.APPLICATION_XML_VALUE])
+    fun listarXml(): List<Cozinha> {
+        println("listarXml()")
         return cozinhaRepository.listar()
     }
 }
