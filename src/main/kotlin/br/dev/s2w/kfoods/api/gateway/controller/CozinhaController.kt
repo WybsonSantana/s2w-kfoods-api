c5f6e6d6a994f2c4c0794076c4456a653eb6b201
WybsonSantana <wybson@hotmail.com>
2023-01-06 09:00:41 -0300

    kFoods - 04.14 - Consultando Singleton Resource com GET e @PathVariable

--- src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
@@ -2,26 +2,25 @@ package br.dev.s2w.kfoods.api.gateway.controller
 
 import br.dev.s2w.kfoods.api.domain.model.Cozinha
 import br.dev.s2w.kfoods.api.domain.repository.CozinhaRepository
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/cozinhas") /* , [produces = MediaType.APPLICATION_XML_VALUE]) */
+@RequestMapping("/cozinhas")
 class CozinhaController(
     private val cozinhaRepository: CozinhaRepository
 ) {
 
-    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun listarJson(): List<Cozinha> {
-        println("listarJson()")
+    @GetMapping
+    fun listar(): List<Cozinha> {
         return cozinhaRepository.listar()
     }
 
-    @GetMapping(produces = [MediaType.APPLICATION_XML_VALUE])
-    fun listarXml(): List<Cozinha> {
-        println("listarXml()")
-        return cozinhaRepository.listar()
+    @GetMapping("/{cozinhaId}")
+    fun buscar(@PathVariable cozinhaId: Long): Cozinha {
+        return cozinhaRepository.buscar(cozinhaId)
     }
+
 }
