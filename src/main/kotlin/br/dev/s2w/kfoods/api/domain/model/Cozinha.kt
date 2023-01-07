3b9ec295b3eb1a064a9b60d5b5eab45a3ee17743
WybsonSantana <wybson@hotmail.com>
2023-01-07 13:30:00 -0300

    kFoods - 04.15 - Customizando as representações XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- src/main/kotlin/br/dev/s2w/kfoods/api/domain/model/Cozinha.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/domain/model/Cozinha.kt
@@ -1,13 +1,19 @@
 package br.dev.s2w.kfoods.api.domain.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonRootName
 import javax.persistence.*
 
+@JsonRootName("Gastronomia")
 @Entity
 data class Cozinha(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
 
+    //@JsonIgnore
+    @JsonProperty("titulo")
     @Column(nullable = false)
     val nome: String
 )
