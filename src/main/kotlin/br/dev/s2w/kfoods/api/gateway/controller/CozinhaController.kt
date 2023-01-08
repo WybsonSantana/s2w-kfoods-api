7ab1079827b4b44c38b9ccf7118debb962e652c9
WybsonSantana <wybson@hotmail.com>
2023-01-08 01:56:15 -0300

    kFoods - 04.26 - Modelando e implementando a exclusão de recursos com DELETE

--- src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/gateway/controller/CozinhaController.kt
@@ -4,6 +4,7 @@ import br.dev.s2w.kfoods.api.domain.model.Cozinha
 import br.dev.s2w.kfoods.api.domain.repository.CozinhaRepository
 import br.dev.s2w.kfoods.api.gateway.model.CozinhasXmlWrapper
 import org.springframework.beans.BeanUtils
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -51,6 +52,22 @@ class CozinhaController(
             ResponseEntity.ok(cozinhaRepository.salvar(cozinhaAtual))
         } else {
             ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/{cozinhaId}")
+    fun remover(@PathVariable cozinhaId: Long): ResponseEntity<Any> {
+        return try {
+            val cozinha = cozinhaRepository.buscar(cozinhaId)
+
+            if (cozinha != null) {
+                cozinhaRepository.remover(cozinha)
+                ResponseEntity.noContent().build()
+            } else {
+                ResponseEntity.notFound().build()
+            }
+        } catch (exception: DataIntegrityViolationException) {
+            ResponseEntity.status(HttpStatus.CONFLICT).build()
         }
     }
 
