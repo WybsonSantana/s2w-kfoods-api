a3f32de445a79ffce3dcf17acf4ad47911a3e337
WybsonSantana <wybson@hotmail.com>
2023-02-18 20:01:48 -0300

    kFoods - 05.10 - Externalizando consultas JPQL para um arquivo XML

--- src/main/kotlin/br/dev/s2w/kfoods/api/domain/repository/RestauranteRepository.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/domain/repository/RestauranteRepository.kt
@@ -2,7 +2,6 @@ package br.dev.s2w.kfoods.api.domain.repository
 
 import br.dev.s2w.kfoods.api.domain.model.Restaurante
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.math.BigDecimal
@@ -11,7 +10,7 @@ import java.math.BigDecimal
 interface RestauranteRepository : JpaRepository<Restaurante, Long> {
     fun findByTaxaFreteBetween(taxaInicial: BigDecimal, taxaFinal: BigDecimal): List<Restaurante>
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+    //@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     fun consultarPorNome(nome: String, @Param("id") cozinhaId: Long): List<Restaurante>
 
     //fun findByNomeContainingAndCozinhaId(nome: String, cozinhaId: Long): List<Restaurante>
