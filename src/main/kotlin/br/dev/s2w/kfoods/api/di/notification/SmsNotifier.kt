eb95af46d82f39593349ca9e42a0c4134691f848
WybsonSantana <wybson@hotmail.com>
2024-12-17 23:29:04 -0300

    KFoods: 02.16 - Bean Ambiguity and Bean List Injection

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/notification/SmsNotifier.kt
@@ -0,0 +1,12 @@
+package br.dev.s2w.kfoods.api.di.notification
+
+import br.dev.s2w.kfoods.api.di.model.Customer
+import org.springframework.stereotype.Component
+
+@Component
+class SmsNotifier : Notifier {
+
+    override fun notify(customer: Customer, message: String) {
+        println("Notifying ${customer.name} via SMS by phone number ${customer.phoneNumber}: $message")
+    }
+}
--- src/main/kotlin/br/dev/s2w/kfoods/api/di/service/CustomerActivationService.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/service/CustomerActivationService.kt
@@ -2,19 +2,17 @@ package br.dev.s2w.kfoods.api.di.service
 
 import br.dev.s2w.kfoods.api.di.model.Customer
 import br.dev.s2w.kfoods.api.di.notification.Notifier
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class CustomerActivationService {
-
-    @Autowired(required = false)
-    private val notifier: Notifier? = null
+class CustomerActivationService(
+    private val notifiers: List<Notifier>
+) {
 
     fun activate(customer: Customer) {
         customer.activate()
 
-        notifier?.notify(customer, "Your registration in the system is active!")
-            ?: println("There is no notifier, but the client has been activated.")
+        for (notifier in notifiers)
+            notifier.notify(customer, "Your registration in the system is active!")
     }
 }
