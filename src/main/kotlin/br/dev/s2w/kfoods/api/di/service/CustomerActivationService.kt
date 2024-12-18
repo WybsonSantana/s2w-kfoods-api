66ec7ac871822fd166b3e11a26ef39c65f5a2531
WybsonSantana <wybson@hotmail.com>
2024-12-17 22:53:13 -0300

    KFoods: 02.15 - Optional dependency with @Autowired

--- src/main/kotlin/br/dev/s2w/kfoods/api/di/service/CustomerActivationService.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/service/CustomerActivationService.kt
@@ -2,23 +2,19 @@ package br.dev.s2w.kfoods.api.di.service
 
 import br.dev.s2w.kfoods.api.di.model.Customer
 import br.dev.s2w.kfoods.api.di.notification.Notifier
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class CustomerActivationService(
-    private val notifier: Notifier
-) {
+class CustomerActivationService {
 
-    //    @Autowired
-//    private lateinit var notifier: Notifier
+    @Autowired(required = false)
+    private val notifier: Notifier? = null
 
     fun activate(customer: Customer) {
         customer.activate()
-        notifier.notify(customer, "Your registration in the system is active!")
-    }
 
-//    @Autowired
-//    fun setNotifier(notifier: Notifier) {
-//        this.notifier = notifier
-//    }
+        notifier?.notify(customer, "Your registration in the system is active!")
+            ?: println("There is no notifier, but the client has been activated.")
+    }
 }
