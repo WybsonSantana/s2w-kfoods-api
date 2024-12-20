df644c06e3f198a5567087c61e392c0672aa416d
WybsonSantana <wybson@hotmail.com>
2024-12-19 22:12:48 -0300

    KFoods: 02.25 - Creating and accessing custom properties with @Value

--- src/main/kotlin/br/dev/s2w/kfoods/api/di/notification/EmailNotifier.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/di/notification/EmailNotifier.kt
@@ -1,13 +1,23 @@
 package br.dev.s2w.kfoods.api.di.notification
 
 import br.dev.s2w.kfoods.api.di.model.Customer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
 @NotifierType(UrgencyLevel.NO_URGENCY)
 class EmailNotifier : Notifier {
 
+    @Value("\${notifier.email.server-host}")
+    private val host: String? = null
+
+    @Value("\${notifier.email.server-port}")
+    private val port: Int? = null
+
     override fun notify(customer: Customer, message: String) {
+        println("Server host: $host")
+        println("Server port: $port")
+
         println("Notifying ${customer.name} by e-mail ${customer.email}: $message")
     }
 
