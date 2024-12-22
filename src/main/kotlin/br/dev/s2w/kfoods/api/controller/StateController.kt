0b1d24a078e6f33f9373bfc48d39f17d704fc2ed
WybsonSantana <wybson@hotmail.com>
2024-12-22 19:28:55 -0300

    KFoods: 04.11 - Challenge: resource collection of states

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/controller/StateController.kt
@@ -0,0 +1,19 @@
+package br.dev.s2w.kfoods.api.controller
+
+import br.dev.s2w.kfoods.api.domain.model.State
+import br.dev.s2w.kfoods.api.domain.repository.StateRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/states")
+class StateController(
+    private val stateRepository: StateRepository
+) {
+
+    @GetMapping
+    fun list(): List<State> =
+        stateRepository.list()
+
+}
