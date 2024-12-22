fed72b3d5a6e5faaa7e8b87c3a5dabbfe46d6767
WybsonSantana <wybson@hotmail.com>
2024-12-22 19:16:32 -0300

    KFoods: 04.10 - Modeling and requesting a Collection Resource with GET

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/controller/CuisineController.kt
@@ -0,0 +1,19 @@
+package br.dev.s2w.kfoods.api.controller
+
+import br.dev.s2w.kfoods.api.domain.model.Cuisine
+import br.dev.s2w.kfoods.api.domain.repository.CuisineRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/cuisines")
+class CuisineController(
+    private val cuisineRepository: CuisineRepository
+) {
+
+    @GetMapping
+    fun list(): List<Cuisine> =
+        cuisineRepository.list()
+
+}
