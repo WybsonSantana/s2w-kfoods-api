16257477c22249dd3c5793356f28516115580fba
WybsonSantana <wybson@hotmail.com>
2024-12-23 13:55:55 -0300

    KFoods: 04.19 - Setting HTTP response status with @ResponseStatus

--- src/main/kotlin/br/dev/s2w/kfoods/api/adapter/controller/CuisineController.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/controller/CuisineController.kt
@@ -3,11 +3,9 @@ package br.dev.s2w.kfoods.api.adapter.controller
 import br.dev.s2w.kfoods.api.adapter.model.CuisinesXmlWrapper
 import br.dev.s2w.kfoods.api.domain.model.Cuisine
 import br.dev.s2w.kfoods.api.domain.repository.CuisineRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/cuisines")
@@ -23,6 +21,7 @@ class CuisineController(
     fun listXml(): CuisinesXmlWrapper =
         CuisinesXmlWrapper(cuisineRepository.list())
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{cuisineId}")
     fun search(@PathVariable cuisineId: Long): Cuisine =
         cuisineRepository.search(cuisineId)
