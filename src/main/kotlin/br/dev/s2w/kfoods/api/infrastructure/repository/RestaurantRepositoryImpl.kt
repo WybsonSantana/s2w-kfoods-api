28276f8a5747d0a6311cdcd9e96441afc1230d72
WybsonSantana <wybson@hotmail.com>
2024-12-29 00:25:16 -0300

    KFoods: 05.13 - Implementing a simple query with Criteria API

--- src/main/kotlin/br/dev/s2w/kfoods/api/infrastructure/repository/RestaurantRepositoryImpl.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/infrastructure/repository/RestaurantRepositoryImpl.kt
@@ -3,7 +3,6 @@ package br.dev.s2w.kfoods.api.infrastructure.repository
 import br.dev.s2w.kfoods.api.domain.model.Restaurant
 import br.dev.s2w.kfoods.api.domain.repository.RestaurantRepositoryQueries
 import org.springframework.stereotype.Repository
-import org.springframework.util.StringUtils
 import java.math.BigDecimal
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
@@ -15,29 +14,12 @@ class RestaurantRepositoryImpl : RestaurantRepositoryQueries {
     private lateinit var manager: EntityManager
 
     override fun find(name: String?, initialFee: BigDecimal?, finalFee: BigDecimal?): List<Restaurant> {
-        val jpql = StringBuilder()
-        val parameters = HashMap<String, Any>()
+        val builder = manager.criteriaBuilder
+        val criteria = builder.createQuery(Restaurant::class.java)
 
-        jpql.append("from Restaurant where 0 = 0 ")
+        criteria.from(Restaurant::class.java)
 
-        if (StringUtils.hasLength(name)) {
-            jpql.append("and name like :name ")
-            parameters["name"] = "%$name%"
-        }
-
-        if (initialFee != null) {
-            jpql.append("and deliveryFee >= :initialFee ")
-            parameters["initialFee"] = initialFee
-        }
-
-        if (finalFee != null) {
-            jpql.append("and deliveryFee <= :finalFee ")
-            parameters["finalFee"] = finalFee
-        }
-
-        val query = manager.createQuery(jpql.toString(), Restaurant::class.java)
-
-        parameters.forEach { (key: String?, value: Any?) -> query.setParameter(key, value) }
+        val query = manager.createQuery(criteria)
 
         return query.resultList
     }
