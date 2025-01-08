6e9d31316e6037e62bf8d4ab4b41a4d8f164e66d
WybsonSantana <wybson@hotmail.com>
2025-01-08 01:15:26 -0300

    KFoods: 08.16 - Customizing the default ResponseEntityExceptionHandler response body

--- src/main/kotlin/br/dev/s2w/kfoods/api/adapter/exceptionhandler/AdapterExceptionHandler.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/exceptionhandler/AdapterExceptionHandler.kt
@@ -3,44 +3,67 @@ package br.dev.s2w.kfoods.api.adapter.exceptionhandler
 import br.dev.s2w.kfoods.api.domain.exception.BusinessException
 import br.dev.s2w.kfoods.api.domain.exception.EntityInUseException
 import br.dev.s2w.kfoods.api.domain.exception.EntityNotFoundException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.time.LocalDateTime
 
 @ControllerAdvice
 class AdapterExceptionHandler : ResponseEntityExceptionHandler() {
 
-    @ExceptionHandler(BusinessException::class)
-    fun handlerBusinessException(e: BusinessException): ResponseEntity<Any> {
-        val problem = Problem(
-            timestamp = LocalDateTime.now(),
-            message = e.message.toString()
-        )
+    override fun handleExceptionInternal(
+        e: Exception,
+        body: Any?,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        lateinit var problem: Problem
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem)
+        if (body == null) {
+            problem = Problem(
+                timestamp = LocalDateTime.now(),
+                message = status.reasonPhrase
+            )
+        } else if (body is String) {
+            problem = Problem(
+                timestamp = LocalDateTime.now(),
+                message = body
+            )
+        }
+
+        return super.handleExceptionInternal(e, problem, headers, status, request)
+    }
+
+    @ExceptionHandler(BusinessException::class)
+    fun handleBusinessException(e: BusinessException, request: WebRequest): ResponseEntity<Any> {
+        val problem = e.message
+        val headers = HttpHeaders()
+        val status = HttpStatus.BAD_REQUEST
+
+        return handleExceptionInternal(e, problem, headers, status, request)
     }
 
     @ExceptionHandler(EntityInUseException::class)
-    fun handlerEntityInUseException(e: EntityInUseException): ResponseEntity<Any> {
-        val problem = Problem(
-            timestamp = LocalDateTime.now(),
-            message = e.message.toString()
-        )
+    fun handleEntityInUseException(e: EntityInUseException, request: WebRequest): ResponseEntity<Any> {
+        val problem = e.message
+        val headers = HttpHeaders()
+        val status = HttpStatus.CONFLICT
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problem)
+        return handleExceptionInternal(e, problem, headers, status, request)
     }
 
     @ExceptionHandler(EntityNotFoundException::class)
-    fun handlerEntityNotFoundException(e: EntityNotFoundException): ResponseEntity<Any> {
-        val problem = Problem(
-            timestamp = LocalDateTime.now(),
-            message = e.message.toString()
-        )
+    fun handleEntityNotFoundException(e: EntityNotFoundException, request: WebRequest): ResponseEntity<Any> {
+        val problem = e.message
+        val headers = HttpHeaders()
+        val status = HttpStatus.NOT_FOUND
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem)
+        return handleExceptionInternal(e, problem, headers, status, request)
     }
 
 }
