ddcad421ee5f6eb63eef62419667a3bfa46213a5
WybsonSantana <wybson@hotmail.com>
2025-01-31 00:50:59 -0300

    KFoods: 10.03 - Creating and running an integration test with Spring Boot, Junit and Assertj

---
+++ src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIntegrationTest.kt
@@ -0,0 +1,48 @@
+package br.dev.s2w.kfoods.api
+
+import br.dev.s2w.kfoods.api.domain.model.Cuisine
+import br.dev.s2w.kfoods.api.domain.service.CuisineRegisterService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import javax.validation.ConstraintViolationException
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+class CuisineRegisterIntegrationTest {
+
+    @Autowired
+    private lateinit var cuisineRegister: CuisineRegisterService
+
+    @Test
+    fun `test cuisine registration successfully`() {
+        // scenario
+        val cuisine = Cuisine(name = "Chinesa")
+
+        // action
+        cuisineRegister.save(cuisine).also { newCuisine ->
+
+            // validation
+            assertThat(newCuisine.id).isNotNull()
+        }
+    }
+
+    @Test
+    fun `test cuisine registration without name`() {
+        // scenario
+        val cuisine = Cuisine(name = null)
+
+        // action
+        val expectedError = assertThrows<ConstraintViolationException> {
+            cuisineRegister.save(cuisine)
+        }
+
+        // validation
+        assertThat(expectedError).isNotNull()
+    }
+
+}
