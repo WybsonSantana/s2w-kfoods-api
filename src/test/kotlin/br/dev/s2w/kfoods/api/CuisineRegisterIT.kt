26fbdfd737a5386b89ef03fac299e4b33c31e6f4
WybsonSantana <wybson@hotmail.com>
2025-02-01 19:50:14 -0300

    KFoods: 10.08 - Implementing API Tests with REST Assured and validating the HTTP status code

--- src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
+++ src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
@@ -1,59 +1,34 @@
 package br.dev.s2w.kfoods.api
 
-import br.dev.s2w.kfoods.api.domain.exception.EntityInUseException
-import br.dev.s2w.kfoods.api.domain.exception.EntityNotFoundException
-import br.dev.s2w.kfoods.api.domain.model.Cuisine
-import br.dev.s2w.kfoods.api.domain.service.CuisineRegisterService
-import org.assertj.core.api.Assertions.assertThat
+import io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import javax.validation.ConstraintViolationException
 
 @ExtendWith(SpringExtension::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CuisineRegisterIT {
 
-    @Autowired
-    private lateinit var cuisineRegister: CuisineRegisterService
+    @LocalServerPort
+    private val port: Int = 0
 
     @Test
-    fun `should assign id when registering kitchen with correct data`() {
-        val cuisine = Cuisine(name = "Chinesa")
+    fun `should return status 200 when querying cuisines`() {
+        enableLoggingOfRequestAndResponseIfValidationFails()
 
-        cuisineRegister.save(cuisine).also { newCuisine ->
-            assertThat(newCuisine.id).isNotNull()
-        }
-    }
-
-    @Test
-    fun `should fail when registering cuisine without name`() {
-        val cuisine = Cuisine(name = null)
-
-        assertThrows<ConstraintViolationException> {
-            cuisineRegister.save(cuisine)
-        }
-    }
-
-    @Test
-    fun `should fail when deleting cuisine in use`() {
-        val cuisineId = 1L
-
-        assertThrows<EntityInUseException> {
-            cuisineRegister.remove(cuisineId)
-        }
-    }
-
-    @Test
-    fun `should fail when deleting cuisine not found`() {
-        val cuisineId = 100L
-
-        assertThrows<EntityNotFoundException> {
-            cuisineRegister.remove(cuisineId)
-        }
+        given()
+            .basePath("/cuisines")
+            .port(port)
+            .accept(ContentType.JSON)
+            .`when`()
+            .get()
+            .then()
+            .statusCode(HttpStatus.OK.value())
     }
 
 }
