5c84828dd521803dc380de486bee6926318ed17a
WybsonSantana <wybson@hotmail.com>
2025-02-01 23:50:54 -0300

    KFoods: 10.12 - Returning the initial state of the database for each test execution with Flyway Callback

--- src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
+++ src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
@@ -2,11 +2,13 @@ package br.dev.s2w.kfoods.api
 
 import io.restassured.RestAssured.*
 import io.restassured.http.ContentType
+import org.flywaydb.core.Flyway
 import org.hamcrest.Matchers.hasItems
 import org.hamcrest.Matchers.hasSize
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
@@ -19,11 +21,16 @@ class CuisineRegisterIT {
     @LocalServerPort
     private val serverPort: Int = 0
 
+    @Autowired
+    private lateinit var flyway: Flyway
+
     @BeforeEach
     fun setup() {
         enableLoggingOfRequestAndResponseIfValidationFails()
         port = serverPort
         basePath = "/cuisines"
+
+        flyway.migrate()
     }
 
     @Test
