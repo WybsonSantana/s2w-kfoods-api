138d2f24e87d960dd169b0a075191891a72c8cd6
WybsonSantana <wybson@hotmail.com>
2025-02-02 10:56:03 -0300

    KFoods: 10.14 - Cleaning and populating the test database

---
+++ src/test/resources/db/integration-testdata/afterMigrate.sql
@@ -0,0 +1,29 @@
+set foreign_key_checks = 0;
+
+delete from city;
+delete from cuisine;
+delete from identity;
+delete from identity_role;
+delete from payment_method;
+delete from permission;
+delete from product;
+delete from restaurant;
+delete from restaurant_payment_method;
+delete from role;
+delete from role_permission;
+delete from state;
+
+set foreign_key_checks = 1;
+
+alter table city auto_increment = 1;
+alter table cuisine auto_increment = 1;
+alter table identity auto_increment = 1;
+alter table payment_method auto_increment = 1;
+alter table permission auto_increment = 1;
+alter table product auto_increment = 1;
+alter table restaurant auto_increment = 1;
+alter table role auto_increment = 1;
+alter table state auto_increment = 1;
+
+insert into cuisine(id, name) values(1, 'Tailandesa');
+insert into cuisine(id, name) values(2, 'Indiana');
