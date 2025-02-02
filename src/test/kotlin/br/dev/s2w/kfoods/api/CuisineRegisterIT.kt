4a03dc684e503852867c274c9fb42348fbfb194e
WybsonSantana <wybson@hotmail.com>
2025-02-02 10:28:08 -0300

    KFoods: 10.13 - Setting up a test bench and using @TestPropertySource

--- src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
+++ src/test/kotlin/br/dev/s2w/kfoods/api/CuisineRegisterIT.kt
@@ -12,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CuisineRegisterIT {
 
     @LocalServerPort
