58d6598e18f3aa54de0efe9badee8b4204d55d04
WybsonSantana <wybson@hotmail.com>
2025-02-10 22:04:17 -0300

    KFoods: 11.03 - Creating Mixin classes to use Jackson's annotations ***

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/model/mixin/RestaurantMixin.kt
@@ -0,0 +1,29 @@
+package br.dev.s2w.kfoods.api.adapter.model.mixin
+
+import br.dev.s2w.kfoods.api.domain.model.Address
+import br.dev.s2w.kfoods.api.domain.model.Cuisine
+import br.dev.s2w.kfoods.api.domain.model.PaymentMethod
+import br.dev.s2w.kfoods.api.domain.model.Product
+import com.fasterxml.jackson.annotation.JsonIgnore
+import java.time.LocalDateTime
+
+class RestaurantMixin(
+    //@JsonIgnoreProperties(value = ["name"], allowGetters = true)
+    // The annotation @JsonIgnoreProperties has no cascade effect on Kotlin
+    val cuisine: Cuisine? = null,
+
+    @JsonIgnore
+    val address: Address? = null,
+
+    @JsonIgnore
+    val registrationDate: LocalDateTime? = null,
+
+    @JsonIgnore
+    val lastUpdateDate: LocalDateTime? = null,
+
+    @JsonIgnore
+    val paymentMethods: MutableList<PaymentMethod> = mutableListOf(),
+
+    @JsonIgnore
+    val products: MutableList<Product> = mutableListOf()
+)
--- src/main/kotlin/br/dev/s2w/kfoods/api/domain/model/Restaurant.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/domain/model/Restaurant.kt
@@ -1,7 +1,6 @@
 package br.dev.s2w.kfoods.api.domain.model
 
 import br.dev.s2w.kfoods.api.core.validation.Groups.RestaurantRegistration
-import com.fasterxml.jackson.annotation.JsonIgnore
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.math.BigDecimal
@@ -27,29 +26,23 @@ data class Restaurant(
     @Column(name = "delivery_fee", nullable = false)
     val deliveryFee: BigDecimal? = null,
 
-    //@JsonIgnoreProperties(value = ["name"], allowGetters = true)
-    // The annotation @JsonIgnoreProperties has no cascade effect on Kotlin
     @field:Valid
     @field:NotNull(groups = [RestaurantRegistration::class])
     @ManyToOne
     @JoinColumn(name = "cuisine_id", nullable = false)
     val cuisine: Cuisine? = null,
 
-    @JsonIgnore
     @Embedded
     val address: Address? = null,
 
-    @JsonIgnore
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     val registrationDate: LocalDateTime? = null,
 
-    @JsonIgnore
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
     val lastUpdateDate: LocalDateTime? = null,
 
-    @JsonIgnore
     @ManyToMany
     @JoinTable(
         name = "restaurant_payment_method",
@@ -58,7 +51,6 @@ data class Restaurant(
     )
     val paymentMethods: MutableList<PaymentMethod> = mutableListOf(),
 
-    @JsonIgnore
     @OneToMany(mappedBy = "restaurant")
     val products: MutableList<Product> = mutableListOf()
 )
