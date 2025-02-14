4f5cb0588154e3a7eae76aa3b58a5148d6ff25b5
WybsonSantana <wybson@hotmail.com>
2025-02-13 23:31:48 -0300

    KFoods: 11.09 - Isolating the Domain Model from the Representation Model with the DTO pattern

---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/model/CuisineModel.kt
@@ -0,0 +1,6 @@
+package br.dev.s2w.kfoods.api.adapter.model
+
+data class CuisineModel(
+    val id: Long? = null,
+    val name: String? = null
+)
---
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/model/RestaurantModel.kt
@@ -0,0 +1,10 @@
+package br.dev.s2w.kfoods.api.adapter.model
+
+import java.math.BigDecimal
+
+data class RestaurantModel(
+    val id: Long? = null,
+    val name: String? = null,
+    val deliveryFee: BigDecimal? = null,
+    val cuisine: CuisineModel? = null
+)
