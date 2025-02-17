02de425f4e0de650ed59b3e206d53c135721193b
WybsonSantana <wybson@hotmail.com>
2025-02-17 00:24:13 -0300

    KFoods: 11.10 - Implementing the conversion of entity to DTO

--- src/main/kotlin/br/dev/s2w/kfoods/api/adapter/controller/RestaurantController.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/controller/RestaurantController.kt
@@ -1,5 +1,8 @@
 package br.dev.s2w.kfoods.api.adapter.controller
 
+import br.dev.s2w.kfoods.api.adapter.model.RestaurantModel
+import br.dev.s2w.kfoods.api.adapter.model.toCollectionModel
+import br.dev.s2w.kfoods.api.adapter.model.toModel
 import br.dev.s2w.kfoods.api.core.validation.Groups
 import br.dev.s2w.kfoods.api.domain.exception.BusinessException
 import br.dev.s2w.kfoods.api.domain.exception.CuisineNotFoundException
@@ -18,18 +21,18 @@ class RestaurantController(
 ) {
 
     @GetMapping
-    fun list(): List<Restaurant> =
-        restaurantRepository.findAll()
+    fun list(): List<RestaurantModel> =
+        restaurantRepository.findAll().toCollectionModel()
 
     @GetMapping("/{restaurantId}")
-    fun find(@PathVariable restaurantId: Long): Restaurant =
-        restaurantRegister.find(restaurantId)
+    fun find(@PathVariable restaurantId: Long): RestaurantModel =
+        restaurantRegister.find(restaurantId).toModel()
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun add(@RequestBody @Validated(Groups.RestaurantRegistration::class) restaurant: Restaurant): Restaurant =
+    fun add(@RequestBody @Validated(Groups.RestaurantRegistration::class) restaurant: Restaurant): RestaurantModel =
         try {
-            restaurantRegister.save(restaurant)
+            restaurantRegister.save(restaurant).toModel()
         } catch (e: CuisineNotFoundException) {
             throw BusinessException(e.message, e)
         }
@@ -38,15 +41,15 @@ class RestaurantController(
     fun update(
         @PathVariable restaurantId: Long,
         @RequestBody @Validated(Groups.RestaurantRegistration::class) restaurant: Restaurant
-    ): Restaurant =
+    ): RestaurantModel =
         try {
-            restaurantRegister.find(restaurantId).also { currentRestaurant ->
+            restaurantRegister.find(restaurantId).let { currentRestaurant ->
                 currentRestaurant.copy(
                     name = restaurant.name,
                     deliveryFee = restaurant.deliveryFee,
                     cuisine = restaurant.cuisine
                 ).let { updatedRestaurant ->
-                    restaurantRegister.save(updatedRestaurant)
+                    restaurantRegister.save(updatedRestaurant).toModel()
                 }
             }
         } catch (e: CuisineNotFoundException) {
--- src/main/kotlin/br/dev/s2w/kfoods/api/adapter/model/RestaurantModel.kt
+++ src/main/kotlin/br/dev/s2w/kfoods/api/adapter/model/RestaurantModel.kt
@@ -1,5 +1,6 @@
 package br.dev.s2w.kfoods.api.adapter.model
 
+import br.dev.s2w.kfoods.api.domain.model.Restaurant
 import java.math.BigDecimal
 
 data class RestaurantModel(
@@ -8,3 +9,15 @@ data class RestaurantModel(
     val deliveryFee: BigDecimal? = null,
     val cuisine: CuisineModel? = null
 )
+
+fun Restaurant.toModel() = RestaurantModel(
+    id = this.id,
+    name = this.name,
+    deliveryFee = this.deliveryFee,
+    cuisine = CuisineModel(
+        id = this.cuisine?.id,
+        name = this.cuisine?.name
+    )
+)
+
+fun List<Restaurant>.toCollectionModel() = this.map { it.toModel() }
